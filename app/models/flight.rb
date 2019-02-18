f198a17542e5b658ecd628eefcfd794f67c78b1d
Tamara Schmidt <tamarafrrr@gmail.com>
2019-02-18 19:19:04 -0300

    Validations Added

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,4 +3,7 @@ class Flight < ApplicationRecord
   belongs_to :order
   has_many :facilities
   has_many :images
+
+  validates :destiny, :description, :origin, presence: true
+  validates :date, :capacity, :price, presence: true
 end
