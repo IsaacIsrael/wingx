9f6dd48051b55a265fc7ccded48c73f4d1beae7c
Tamara Schmidt <tamarafrrr@gmail.com>
2019-02-18 19:15:25 -0300

    Controllers Added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,25 @@
 Rails.application.routes.draw do
+  get 'companies/new'
+  get 'companies/create'
+  get 'companies/edit'
+  get 'companies/update'
+  get 'companies/destroy'
+  get 'orders/index'
+  get 'orders/show'
+  get 'orders/new'
+  get 'orders/create'
+  get 'flights/index'
+  get 'flights/show'
+  get 'flights/new'
+  get 'flights/create'
+  get 'flights/edit'
+  get 'flights/update'
+  get 'flights/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :company do
+    resources :flight
+  end
+  resources :order, only: [:index, :show, :new, :create]
 end
