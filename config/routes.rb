ec1ebb1038cfd63ca13574f28283244b6852a1e9
isaac israel <email.iisrael@gmail.com>
2019-02-19 14:20:19 -0300

    Update the routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :companies, except: [:show] do
+  resources :companies, only: [:new, :create] do
     resources :flights
   end
   resources :orders, only: [:index, :show, :new, :create]
