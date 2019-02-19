abcc8127ab3bb18b37fdf18fe00d1a163fa1283b
Henrique Medeiros <hmdros@gmail.com>
2019-02-19 11:47:05 -0300

    Livereload added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,3 +62,10 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
+
+MyApp::Application.configure do
+  # Add Rack::LiveReload to the bottom of the middleware stack with the default options:
+  config.middleware.insert_after ActionDispatch::Static, Rack::LiveReload
+
+  # ...
+end
