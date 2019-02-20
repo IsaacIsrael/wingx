2a0d9f2c1d01ca453d83c5a1148f71cf60a6652e
Tamara Schmidt <tamarafrrr@gmail.com>
2019-02-20 15:48:02 -0300

    View and Pundit Added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  devise_for :controllers
+  devise_for :views
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -7,4 +10,6 @@ Rails.application.routes.draw do
   get 'company', to: 'companies#show', as: :company
   resources :flights
   resources :orders, only: [:index, :show, :new, :create]
+  get 'user/:id', to: 'users#show'
+  # resources :users, only: [:show]
 end
