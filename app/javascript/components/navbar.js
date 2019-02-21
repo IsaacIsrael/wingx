091c165c36238ccb0efb16cae3a5efb1c81c4885
Fabricio Bravo <fgbravo188@gmail.com>
2019-02-21 14:43:12 -0300

    Removed devise notice for login and logout so it would work with home navbar

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -1,6 +1,5 @@
 const transparentNavbar = () => {
   if(window.location.pathname == '/') {
-    console.log('awesome');
     const navbar = document.getElementById("navbar");
     navbar.classList.toggle("navbar-wagon-transparent");
   }
