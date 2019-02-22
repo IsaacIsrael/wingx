c10158f8a93a74f782fd1a3c1ea328fc7e4f4036
Henrique Medeiros <hmdros@gmail.com>
2019-02-22 12:09:20 -0300

    price and capacity validations, cant be negative

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -10,9 +10,20 @@ class Flight < ApplicationRecord
   validates :date, :capacity, :price, presence: true
 
   validate :flight_date_cant_be_in_past
+  validate :price_cant_be_negative
+  validate :capacity_cant_be_negative
+
 
   def flight_date_cant_be_in_past
     errors.add(:date, 'can\'t be in the past') if date < Date.today
+  end
+
+  def price_cant_be_negative
+    errors.add(:price, 'can\'t be negative') if price.negative?
+  end
+
+  def capacity_cant_be_negative
+    errors.add(:capacity, 'can\'t be negative') if capacity.negative?
   end
 
   def your_owner?(user)
