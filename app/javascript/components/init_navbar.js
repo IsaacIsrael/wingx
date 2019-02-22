d913078af290b6914b1aeba69c911ccd15e9ec50
Fabricio Bravo <fgbravo188@gmail.com>
2019-02-22 17:15:07 -0300

    Really fixed it this time

--- app/javascript/components/init_navbar.js
+++ app/javascript/components/init_navbar.js
@@ -1,5 +1,4 @@
 const initTransparentNavbar = () => {
-  const navbar = document.getElementById("navbar");
-  navbar.classList.toggle("navbar-wagon-transparent");
+
 }
 export { initTransparentNavbar };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,8 @@
 import "bootstrap";
 
 import { initDisplayValue } from '../components/init_order_component'
-import { initTransparentNavbar } from "../components/init_navbar";
 import { initReadUrl } from "../components/init_preview";
 
 initReadUrl();
-initTransparentNavbar();
 initDisplayValue();
 
