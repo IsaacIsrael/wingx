75c7ad2be8b28e096cddcc0591766bada6a9bea8
Fabricio Bravo <fgbravo188@gmail.com>
2019-02-22 16:53:00 -0300

    Made the logo white in the home page

--- app/javascript/components/init_navbar.js
+++ app/javascript/components/init_navbar.js
@@ -1,6 +1,8 @@
 const initTransparentNavbar = () => {
   if(window.location.pathname == '/') {
     const navbar = document.getElementById("navbar");
+    const logo = document.getElementById("logo");
+    logo.src="/assets/logo-white.png"
     navbar.classList.toggle("navbar-wagon-transparent");
   }
 }
